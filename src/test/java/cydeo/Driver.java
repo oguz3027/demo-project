b87a60fbdf70439e1823f4ce8adda4af815acbed
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-03-01 13:25:01 +0300

    search added

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "3.235.42.200";
+                        String gridAddress = "44.204.197.167";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
